6fc454da7d13a80e796afc39e4537698c923181c
Javi <itsjavii1610@gmail.com>
2026-02-25 10:52:08 +0100

    Pelien

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 import Personajes.Personaje;
 import Personajes.ScarletWitch;
@@ -6,22 +7,24 @@ import Personajes.VictorVonDoom;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
         ScarletWitch wanda = new ScarletWitch("Wanda", 100);
         VictorVonDoom doom = new VictorVonDoom("Doctor Doom", 100, "Energy Blast", "Power up of Molecule Man",
-                120, "Magic Guard" );
-
+                "Magic Guard" );
 
         System.out.println("After the disasters due to incursions around multiverse, there was no hope for our heroes, but at the end of all \n" +
-                "one man stands up against the responsibles of the incursions.Thanks to Doctor Strange and Molecule Man, Victor Von Doom was able to absorb and control \n" +
+                "one man stands up against the responsibles of the incursions. Thanks to Doctor Strange and Molecule Man, Victor Von Doom was able to absorb and control \n" +
                 "one of the biggest powers on the multiverse, Beyonders power.");
+
         System.out.println("Suddenly, there was no caos, only a world reigned by Victor Von Doom.");
-        System.out.println("Doctor Doom, with Beyonder's power, created ");
+        System.out.println("Doom, with Beyonder's power, created \n");
 
         System.out.println("####    ###   #####  #####  #      #####  #     #   ###   ####   #      #### ");
         System.out.println("#   #  #   #    #      #    #      #      #     #  #   #  #   #  #      #   #");
         System.out.println("####   #####    #      #    #      ###    #  #  #  #   #  ####   #      #   #");
         System.out.println("#   #  #   #    #      #    #      #      ## # ##  #   #  # #    #      #   #");
-        System.out.println("####   #   #    #      #    #####  #####   #   #   ###   #  #   #####   #### ");
+        System.out.println("####   #   #    #      #    #####  #####   #   #    ###   #  #   #####  #### ");
 
         System.out.println();
 
@@ -31,5 +34,18 @@ public class Main {
 
         int turn = 1;
         boolean battle = true;
+        int whoBegin = random.nextInt(2);
+
+        while (battle) {
+            System.out.println("Turn " + turn);
+
+            if (whoBegin == 0) {
+                System.out.println(wanda.getName() + " starts the battle");
+            } else {
+                System.out.println(doom.getName() + " starts the battle");
+            }
+
+            
+        }
     }
 }
